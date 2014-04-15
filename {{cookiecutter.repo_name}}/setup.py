1454560955debe370062919438c41373ca046698
Thomas Roten <thomas@roten.us>
2014-04-16 06:17:57 +0800

    Updates setup.py.

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -4,22 +4,29 @@
 import os
 import sys
 
-
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+import {{ cookiecutter.repo_name }}
+
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
-readme = open('README.rst').read()
-history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+open_file(filename):
+    """Open and read the file *filename*."""
+    with open(filename) as f:
+        return f.read()
+
+readme = open_file('README.rst')
+history = open_file('HISTORY.rst').replace('.. :changelog:', '')
 
 setup(
     name='{{ cookiecutter.repo_name }}',
-    version='{{ cookiecutter.version }}',
+    version={{ cookiecutter.version }}.__version__,
     description='{{ cookiecutter.project_short_description }}',
     long_description=readme + '\n\n' + history,
     author='{{ cookiecutter.full_name }}',
@@ -32,8 +39,7 @@ setup(
     include_package_data=True,
     install_requires=[
     ],
-    license="MIT",
-    zip_safe=False,
+    license='MIT',
     keywords='{{ cookiecutter.repo_name }}',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
@@ -41,10 +47,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
-    test_suite='tests',
+    test_suite='{{ cookiecutter.repo_name }}.tests',
 )
